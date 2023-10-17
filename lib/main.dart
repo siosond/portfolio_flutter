d89bbe54c205e29846f241790dfe2324da342b4e
Daniel Sioson <dev@siosond.com>
2023-10-17 18:06:32 +0800

    refactor(app): rename app file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:portfolio_flutter/portfolio_app.dart';
+import 'package:portfolio_flutter/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
