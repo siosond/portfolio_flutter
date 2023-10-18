b61747a96a4c9f317c6e6219cdedc3b97a297108
Daniel Sioson <dev@siosond.com>
2023-10-18 17:40:14 +0800

    feat(about): add resume

---
+++ lib/constants/urls.dart
@@ -0,0 +1,3 @@
+class Urls {
+  static const resume = 'https://staging-portfolio.siosond.avalonartisans.online/downloads/resume.pdf';
+}
--- lib/widgets/about/about.dart
+++ lib/widgets/about/about.dart
@@ -1,7 +1,16 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+import 'dart:js' as js;
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_flutter/constants/urls.dart';
+import 'package:portfolio_flutter/widgets/about/skills/skills_list.dart';
+import 'package:portfolio_flutter/widgets/about/skills/skills_row.dart';
 import 'package:portfolio_flutter/widgets/common/noto_sans_text.dart';
+import 'package:portfolio_flutter/widgets/experience/common/experience_header.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class About extends StatelessWidget {
   const About({Key? key}) : super(key: key);
@@ -14,9 +23,9 @@ class About extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: NotoSansText(
-            tr("about"),
+            tr("about_introduction_header"),
             style: GoogleFonts.notoSans(
-              fontSize: 24,
+              fontSize: 28,
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -24,14 +33,16 @@ class About extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: NotoSansText(
-            tr('about_summary'),
+            tr('about_introduction_item'),
             style: GoogleFonts.notoSans(fontSize: 16),
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: FilledButton(
-            onPressed: () {},
+            onPressed: () {
+              js.context.callMethod('open', [Urls.resume]);
+            },
             child: Padding(
               padding: const EdgeInsets.all(4.0),
               child: NotoSansText(
