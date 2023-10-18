15eeb41fbe897bb26e1d2729ef76de25c6b62e6f
Daniel Sioson <dev@siosond.com>
2023-10-18 16:45:25 +0800

    feat(constants): add assets constants

---
+++ lib/constants/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  static const flutter = 'assets/images/skills/flutter.svg';
+  static const java = 'assets/images/skills/java.svg';
+  static const javascript = 'assets/images/skills/javascript.svg';
+  static const kotlin = 'assets/images/skills/kotlin.svg';
+  static const laravel = 'assets/images/skills/laravel.svg';
+}
